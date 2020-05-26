7cad7adb33462888772d7dc28a93b6b4dd446691
lananh1909 <lananha1k17@gmail.com>
2020-05-26 18:41:34 +0700

    LA

--- src/com/process/FormatDate.java
+++ src/com/process/FormatDate.java
@@ -3,7 +3,7 @@ package com.process;
 public class FormatDate {
 	private final String year = "2020";
 	
-	public String FormatDate(String date) {
+	public String formatDate(String date) {
 		String day = date.substring(0, 2);
 		String month = date.substring(2);
 		return day + "-" + month + "-" + year;
--- src/com/sentences/stockCodeExchangeByDay/KhopLenhTrongTuan.java
+++ src/com/sentences/stockCodeExchangeByDay/KhopLenhTrongTuan.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.modelDataCK.StockModel;
+import com.process.FormatDate;
 import com.process.FormatNumber;
 
 // Trong tuan qua, tong khoi luong khop lenh dao dong trong khoang ....
 public class KhopLenhTrongTuan {
 	private List<StockModel> stockModel = new ArrayList<StockModel>();
+	private String maxDate;
+	private String minDate;
 	
 	public KhopLenhTrongTuan (List<StockModel> stockModel) {
 		this.stockModel = stockModel;
@@ -17,8 +20,10 @@ public class KhopLenhTrongTuan {
 	public double findMax() {
 		double max = 0;
 		for(int i = 0; i<stockModel.size(); i++) {
-			if(stockModel.get(i).getSumOrderMatchingMass()>max)
+			if(stockModel.get(i).getSumOrderMatchingMass()>max) {
 				max = stockModel.get(i).getSumOrderMatchingMass();
+				maxDate = stockModel.get(i).getDate();
+			}
 		}
 		return max;
 	}
@@ -26,8 +31,10 @@ public class KhopLenhTrongTuan {
 	public double findMin() {
 		double min = -999999;
 		for(int i = 0; i<stockModel.size(); i++) {
-			if(stockModel.get(i).getSumOrderMatchingMass()<min)
+			if(stockModel.get(i).getSumOrderMatchingMass()<min) {
 				min = stockModel.get(i).getSumOrderMatchingMass();
+				minDate = stockModel.get(i).getDate();
+			}				
 		}
 		return min;
 	}
@@ -37,7 +44,9 @@ public class KhopLenhTrongTuan {
 		String min = new FormatNumber().formatNumber(findMin()*10);		
 		
 		return "Trong tuan vua qua, khoi luong khop lenh dao dong trong khoang tu "
-				+ min + " co phieu den " + max + " co phieu";
+				+ min + " co phieu den " + max + " co phieu; nhieu nhat trong ngay "
+				+ new FormatDate().formatDate(maxDate) + " va it nhat trong ngay "
+				+ new FormatDate().formatDate(minDate) + ".";
 	}
 	
 }
