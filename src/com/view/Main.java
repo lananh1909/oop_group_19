4076ec8b82b9bc5a3931b1e70068603f7b37c189
KhanhKelvin <khahkelvin1412@gmail.com>
2020-06-09 18:38:40 +0700

    add folder view

--- src/com/view/Main.java
+++ src/com/view/Main.java
@@ -1,4 +1,24 @@
 package com.view;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        primaryStage.setTitle("Stock Table month 5");
+        Scene home = new Scene(root);
+        home.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(home);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
