c936161ecc01b8baa4670639cbd358c1d537e752
KhanhKelvin <khahkelvin1412@gmail.com>
2020-06-10 11:17:58 +0700

    khanh

--- src/com/controller/ControllerStockCode.java
+++ src/com/controller/ControllerStockCode.java
@@ -1,8 +1,11 @@
 package com.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +14,35 @@ public class ControllerStockCode extends GenericController implements Initializa
 
     @FXML
     private ComboBox<String> comboBoxFloor;
+
+    @FXML
+    private TextArea code;
+
+    @FXML
+    private Label contentNotify;
+
+    @FXML
+    private TextArea contentCode;
+
+
+    public void buttonSearch(ActionEvent event){
+
+        String floor = comboBoxFloor.getValue();
+        String stockCode = code.getText();
+
+        if(floor != null && stockCode.equals("")){
+            contentCode.setText("Nothing!!");
+        }
+
+        if (floor == null && stockCode.equals("")){
+            contentNotify.setText("Hãy lựa mã chứng khoán để có được thông tin rõ nhất!");
+            StringBuilder stringBuilder = new StringBuilder();
+            contentCode.setText(stringBuilder.append("hello").toString());
+        }
+
+        if(floor != null && !stockCode.equals("")){
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
